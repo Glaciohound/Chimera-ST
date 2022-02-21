18d2a8798caa030de9142220248d549cb9ac9802
Glaciohound <hanchier@gmail.com>
2022-02-20 17:38:59 -0800

    solving issue#3 mill-run

--- chimera/tools/download_wav2vec2.sh
+++ chimera/tools/download_wav2vec2.sh
@@ -1,6 +1,7 @@
 pretrained_ckpt=$1
 WAVE2VEC_DIR=$2
-external_url=http://sf3-ttcdn-tos.pstatp.com/obj/nlp-opensource/acl2021/chimera
+# external_url=http://sf3-ttcdn-tos.pstatp.com/obj/nlp-opensource/acl2021/chimera
+external_url=http://lf3-nlp-opensource.bytetos.com/obj/nlp-opensource/acl2021/chimera
 if [[ ! -f $WAVE2VEC_DIR/$pretrained_ckpt ]]; then
     wget -P $WAVE2VEC_DIR $external_url/$pretrained_ckpt
 fi
